919a0fdf4938838fd8561b0baedf03eebb25365c
Shankar <shangan.23@gmail.com>
2021-06-23 17:07:05 +0530

    tech(function/lifecycle): add function lifecycle by example

---
+++ src/function-lifecycle.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { FunctionBasedLifecycle } from './lifecycle/function-based-lifecycle'
+
+ReactDOM.render(<FunctionBasedLifecycle />, document.getElementById('root'))
